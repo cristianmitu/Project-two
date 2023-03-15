57effdce06f9c7f70fe876087dc277e66cc77735
Cristian Mitu <mitu_christian@yahoo.com>
2023-03-15 15:52:39 +0000

    added search bar in app.js

--- src/App.js
+++ src/App.js
@@ -21,5 +21,40 @@ function App() {
     </div>
   );
 }
+import ReactNativeInputSearchBar from 'react-native-input-search-bar'
+import { useState } from 'react'
+
+const SearchBar = () => {
+  const [query, setQuery] = useState<string>('')
+  const [activeSearch, setActiveSearch] = useState<boolean>(false)
+
+  const onSubmitSearch = (val: string) => {
+    setQuery(val)
+  }
+
+  return (
+    <ReactNativeInputSearchBar
+      onSubmitSearch={onSubmitSearch}
+      onActiveSearch={setActiveSearch}
+      inputTextStyle={{}}
+      buttonStyle={{
+        paddingHorizontal: 20
+      }}
+      buttonTextStyle={{}}
+      searchToolContainerStyle={{ height: 40 }}
+      inputContainerStyle={{
+        backgroundColor: '#ffffff',
+        borderWidth: 0.3,
+        borderRadius: 20
+      }}
+      inputProps={{
+        placeholder: 'Please enter your search query'
+      }}
+      buttonText="Search"
+    />
+  )
+}
+
+export default SearchBar
 
 export default App;
