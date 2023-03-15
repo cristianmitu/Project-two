3cf62d44a67183f7c6b53f9b5b2b8044a1871d37
Cristian Mitu <mitu_christian@yahoo.com>
2023-03-15 12:36:47 +0000

    retrieving data from You Tube API

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,19 @@
-const key = "79a50b21902c8d9bd27a20543dc2f4c5";
-// let apiLink = ;
+// Movie database API key : https://www.themoviedb.org/  
+const movieDataBaseKey = "79a50b21902c8d9bd27a20543dc2f4c5";
+// You tube API key : https://console.cloud.google.com/apis/credentials?project=projectone-376623
+const youTubeKey = "AIzaSyAIiJIzXXZuhC176V9ed3Vso1B3NU-CdQ8";
 
 
 
 
-fetch("https://api.themoviedb.org/3/search/person?api_key=" + key + "&language=en-US&query=tom&page=1&include_adult=false")
+fetch("https://api.themoviedb.org/3/search/person?api_key=" + movieDataBaseKey + "&language=en-US&query=tom&page=1&include_adult=false")
     .then(response => response.json())
     .then(data => {
         console.log(data);
     })
+    
+fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&eventType=none&maxResults=10&order=viewCount&q=Sandra%20Bullock&videoType=any&key=" + youTubeKey)
+.then(response => response.json())
+.then(data => {
+    console.log(data);
+})
