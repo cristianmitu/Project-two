06bb9ba532baa9f1cd7b39c6572387efb75d210e
Cristian Mitu <mitu_christian@yahoo.com>
2023-03-16 14:43:34 +0000

    got website rendering

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+// import SearchBar from './components/searchbar';
 
 function App() {
   return (
     <div className="App">
-
+      {/* <SearchBar></SearchBar> */}
       <h2>MOVIE TITLE</h2>
       <img src="www.getlinkfromapiconnection.com" alt="example"></img>
 
@@ -13,40 +14,5 @@ function App() {
     </div>
   );
 }
-import ReactNativeInputSearchBar from 'react-native-input-search-bar'
-import { useState } from 'react'
-
-const SearchBar = () => {
-  const [query, setQuery] = useState<string>('')
-  const [activeSearch, setActiveSearch] = useState<boolean>(false)
-
-  const onSubmitSearch = (val: string) => {
-    setQuery(val)
-  }
-
-  return (
-    <ReactNativeInputSearchBar
-      onSubmitSearch={onSubmitSearch}
-      onActiveSearch={setActiveSearch}
-      inputTextStyle={{}}
-      buttonStyle={{
-        paddingHorizontal: 20
-      }}
-      buttonTextStyle={{}}
-      searchToolContainerStyle={{ height: 40 }}
-      inputContainerStyle={{
-        backgroundColor: '#ffffff',
-        borderWidth: 0.3,
-        borderRadius: 20
-      }}
-      inputProps={{
-        placeholder: 'Please enter your search query'
-      }}
-      buttonText="Search"
-    />
-  )
-}
-
-export default SearchBar
 
 export default App;
