9f19fa7c4f89d2ae769bc8fb78d2cbe1eeb7983b
barrymoore1990 <hello@touringhighlights.com>
2023-03-16 15:49:43 +0000

    added more components

--- src/App.js
+++ src/App.js
@@ -1,52 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Search from './pages/Search';
+import About from './pages/About';
+import Header from './components/header/header';
+import Navbar from './components/navbar/navbar';
+import Footer from './components/footer/footer';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <Navbar />
+        <Routes>
+          <Route path="/" element={<Search/>} />
+          <Route path="/about" element={<About/>} />
 
-      <h2>MOVIE TITLE</h2>
-      <img src="www.getlinkfromapiconnection.com" alt="example"></img>
-
-     <h1> Test </h1>
-
-    </div>
+          
+      </Routes>
+    </Router>
   );
 }
-import ReactNativeInputSearchBar from 'react-native-input-search-bar'
-import { useState } from 'react'
-
-const SearchBar = () => {
-  const [query, setQuery] = useState<string>('')
-  const [activeSearch, setActiveSearch] = useState<boolean>(false)
-
-  const onSubmitSearch = (val: string) => {
-    setQuery(val)
-  }
-
-  return (
-    <ReactNativeInputSearchBar
-      onSubmitSearch={onSubmitSearch}
-      onActiveSearch={setActiveSearch}
-      inputTextStyle={{}}
-      buttonStyle={{
-        paddingHorizontal: 20
-      }}
-      buttonTextStyle={{}}
-      searchToolContainerStyle={{ height: 40 }}
-      inputContainerStyle={{
-        backgroundColor: '#ffffff',
-        borderWidth: 0.3,
-        borderRadius: 20
-      }}
-      inputProps={{
-        placeholder: 'Please enter your search query'
-      }}
-      buttonText="Search"
-    />
-  )
-}
-
-export default SearchBar
 
 export default App;
