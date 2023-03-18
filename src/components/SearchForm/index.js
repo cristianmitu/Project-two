fab2847c38e0a3db09697817bf46d3b035da11d6
barrymoore1990 <hello@touringhighlights.com>
2023-03-18 05:36:39 +0000

    basic page structure and navigation displaying

---
+++ src/components/SearchForm/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// Using the datalist element we can create autofill suggestions based on the props.breeds array
+function SearchForm(props) {
+    return (
+      <form>
+        <input type="text" id="name" name="name"></input>
+        <input type="submit" id="submit" value="submit"></input>
+        
+      </form>
+      
+    );
+  }
+  
+  export default SearchForm;
