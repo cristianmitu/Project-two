9558544c614542af6e0c168ebfeb3f81ab8dd6c3
Cristian Mitu <mitu_christian@yahoo.com>
2023-03-18 21:30:20 +0000

    styling page tested

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -1,19 +1,21 @@
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Button from 'react-bootstrap/Button';
+import "./style.css"; 
 
-function SearchForm() {
+function SearchForm() {  
   return (
-    <>
+    <div className="container" id="searchForm">
+      <h2>Search for your favourite actor</h2>
       <InputGroup size="lg">
         <Form.Control
           aria-label="Large"
           aria-describedby="inputGroup-sizing-sm"
         />
       </InputGroup>
-      <Button variant="primary">Search</Button>{' '}
+      <Button variant="primary" id="searchFormBtn">Search</Button>{' '}
 
-    </>
+    </div>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ import Trending from "./../components/Trending"
 function Home() {
     return (
         <div>
-            <h2>Search for your favourite actor</h2>
+            {/* <h2>Search for your favourite actor</h2> */}
             <SearchForm />
             <Trending />
         </div>
