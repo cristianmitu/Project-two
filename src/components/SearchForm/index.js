55ba7b9eb1e8e4c72a7bb4fe65d99d6c20a985a2
barrymoore1990 <hello@touringhighlights.com>
2023-03-19 13:28:47 +0000

    added search button onclick feature

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -1,19 +1,41 @@
+import React, { useState } from "react";
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Button from 'react-bootstrap/Button';
 
 function SearchForm() {
+  
+  const [state, setState] = useState("")
+
+  const handleInputChange = (event) => {
+    const { value } = event.target;
+    setState(() => (value));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(state);
+  };
+  
   return (
+    <form onSubmit={handleSubmit}>
     <>
-      <InputGroup size="lg">
+      <InputGroup size="lg"
+          type="text"
+          name="actorSearch"
+          value={state}
+          onChange={handleInputChange}
+      >
         <Form.Control
+          
           aria-label="Large"
           aria-describedby="inputGroup-sizing-sm"
         />
       </InputGroup>
-      <Button variant="primary">Search</Button>{' '}
+      <Button variant="primary" type="submit">Search</Button>{' '}
 
     </>
+    </form>
   );
 }
 
