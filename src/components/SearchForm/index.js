fcc63b85f20f9c6cfe2f913c2131679492123cf3
Chidi Nwanneka <cnwanneka@gmail.com>
2023-03-20 13:33:09 +0000

    added search.js page

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -1,21 +1,52 @@
-import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
-import Button from 'react-bootstrap/Button';
-import "./style.css"; 
+//import Form from 'react-bootstrap/Form';
+//import InputGroup from 'react-bootstrap/InputGroup';
+//import Button from 'react-bootstrap/Button';
+//import "./style.css"; 
 
-function SearchForm() {  
+//function SearchForm() {  
+  //return (
+    //<div className="container" id="searchForm">
+      //<h2>Search for your favourite actor</h2>
+      //<InputGroup size="lg">
+        //<Form.Control
+          //aria-label="Large"
+          //aria-describedby="inputGroup-sizing-sm"
+        ///>
+      //</InputGroup>
+      //<Button variant="primary" id="searchFormBtn">Search</Button>{' '}
+
+    //</div>
+  //);
+//}
+
+//export default SearchForm;
+
+
+import React from "react";
+import "./style.css";
+
+// Using the datalist element we can create autofill suggestions based on the props.breeds array
+function SearchForm(props) {
   return (
-    <div className="container" id="searchForm">
-      <h2>Search for your favourite actor</h2>
-      <InputGroup size="lg">
-        <Form.Control
-          aria-label="Large"
-          aria-describedby="inputGroup-sizing-sm"
+    <form className="search">
+      <div className="form-group">
+        <label htmlFor="actor">Actor name:</label>
+        <input
+          value={props.search}
+          onChange={props.handleInputChange}
+          name="actor"
+          list="actors"
+          type="text"
+          className="form-control"
+          placeholder="Type in an actor's name to begin"
+          id="actor"
         />
-      </InputGroup>
-      <Button variant="primary" id="searchFormBtn">Search</Button>{' '}
-
-    </div>
+        
+        <button type="submit" onClick={props.handleFormSubmit} className="btn btn-success">
+          Search
+        </button>
+      </div>
+    </form>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,13 @@
 import React from "react"
 import SearchForm from "./../components/SearchForm"
-import Trending from "./../components/Trending"
+
 
 function Home() {
     return (
         <div>
             {/* <h2>Search for your favourite actor</h2> */}
             <SearchForm />
-            <Trending />
+        
         </div>
     )
 }
